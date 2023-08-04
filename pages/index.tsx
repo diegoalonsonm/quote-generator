7407816d4af3f5aff07370edfb8f88559de81b11
diego <diegoalonsonm@gmail.com>
2023-08-04 10:15:27 -0600

    quote generator initial template

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Head from 'next/head'
 import Clouds1 from '../assets/cloud-and-thunder.png'
 import Clouds2 from '../assets/cloudy-weather.png'
 
-import { BackgroundImage1, BackgroundImage2, FootCont, FooterLink, GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import { BackgroundImage1, BackgroundImage2, FootCont, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubtitle, QuoteGeneratorTitle } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
 export default function Home() {
   const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0)
@@ -20,14 +20,33 @@ export default function Home() {
       </Head>
       
       <GradientBackgroundCon>
+        {/* bg images */}
         <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1"/>
         <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground1"/>
 
+        {/* quote generator modal pop-up*/}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Quote Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubtitle>
+              Click the button to generate a quote
+            </QuoteGeneratorSubtitle>
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Generate Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* footer */}
         <FootCont>
           <>
             Quotes generated: {numberOfQuotes}
             <br/>
-            Developed by <FooterLink href="google.com" target="_blank"
+            Developed by <FooterLink href="https://github.com/diegoalonsonm" target="_blank"
             rel="noopener noreferrer">Diego Naranjo</FooterLink>
           </>
         </FootCont>
